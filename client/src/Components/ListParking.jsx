ccb8fbcceeb883727baa98a2303b5d4a2285bf41
Daivy Morales <daivymorales7705@gmail.com>
2023-02-21 14:45:55 -0500

    Showing the vehicles in a table from End-Point

--- client/src/Components/ListParking.jsx
+++ client/src/Components/ListParking.jsx
@@ -1,3 +1,61 @@
+import { useEffect, useState } from "react";
+import { getVehicles } from "../api/vehicle.api";
+
+import { Vehicle } from "./Vehicle";
+
 export const ListParking = () => {
-  return <div>ListParking</div>;
+  const [vehicle, setVehicle] = useState([]);
+
+  useEffect(() => {
+    const loadVehicle = async () => {
+      const response = await getVehicles();
+      setVehicle(response.data);
+    };
+    loadVehicle();
+  }, []);
+
+  console.log(vehicle);
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <caption className="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+            Our products
+            <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
+              Browse a list of Flowbite products designed to help you work and
+              play, stay organized, get answers, keep in touch, grow your
+              business, and more.
+            </p>
+          </caption>
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                PLACAS
+              </th>
+              <th scope="col" className="px-6 py-3">
+                NOMBRES
+              </th>
+              <th scope="col" className="px-6 py-3">
+                APELLIDOS
+              </th>
+              <th scope="col" className="px-6 py-3">
+                PRODUCTO
+              </th>
+              <th scope="col" className="px-6 py-3">
+                HORA DE ENTRADA
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <span className="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {vehicle.map((vehicle, index) => (
+              <Vehicle vehicle={vehicle} key={index} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
--- client/src/Components/Vehicle.jsx
+++ client/src/Components/Vehicle.jsx
@@ -1,3 +1,36 @@
-export const Vehicle = () => {
-  return <div>Vehicle</div>;
+import { Button } from "flowbite-react";
+import { useState } from "react";
+
+export const Vehicle = ({ vehicle }) => {
+  const [date, setDate] = useState(new Date(vehicle.updatedAt));
+
+  const hour = date.getHours();
+  const minutes = date.getSeconds();
+
+  const showTime = `${hour.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}`;
+
+  return (
+    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+      <th
+        scope="row"
+        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+      >
+        {vehicle.plates}
+      </th>
+      <td className="px-6 py-4">{vehicle.name.toUpperCase()}</td>
+      <td className="px-6 py-4">{vehicle.lastName.toUpperCase()}</td>
+      <td className="px-6 py-4">{vehicle.product}</td>
+      <td className="px-6 py-4">{showTime}</td>
+      <td className="px-6 py-4 text-right">
+        <a
+          href="#"
+          className="font-medium text-red-600 dark:text-blue-500 hover:underline"
+        >
+          Eliminar
+        </a>
+      </td>
+    </tr>
+  );
 };
